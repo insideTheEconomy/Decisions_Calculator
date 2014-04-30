6b9c9acaed9c4a1cc1ddf939588a1e4b901f2092
Ray Ortgiesen <ray.ortgiesen@gmail.com>
2014-04-30 10:33:05 -0400

    Added Car and Couch Scenario

--- Resources/JS/selectors.js
+++ Resources/JS/selectors.js
@@ -3,14 +3,36 @@
 
 //-- Variable Declarations  -- //
 
+var initHTML;
+var initiated;
+
 var scenario;
+var curScreen;
+
+// --------------------------- //
+
+$(function () {
+	//Happens only FIRST time program is run
+	if (!initiated) {
+		console.log("Init");
+		initHTML = $(document.body).html();
+		initiated = true;
+	} else {
+		
+	}
+	
+	//Happens on EVERY restart
+	loadIdle();
+	curScreen = 0;
+});
 
 // --------------------------- //
 
 
+//-- Click Event Handlers  -- //
 
+//Removes the attract loop and animates in the scenarioSelectBtns
 $("#attractLoop").click(function(e) {
-	//$("#attractLoop").slideUp();
 	$( "#attractLoop" ).tween({
 	   opacity:{
 	      start: 100,
@@ -24,16 +46,16 @@ $("#attractLoop").click(function(e) {
 				   opacity:{
 				      start: 0,
 				      stop: 100,
-				      time: i/2,
+				      time: (i/2)-(0.35*i),
 				      duration: 0.25,
 				      effect:'easeInOut'
 				   },
 				   transform:{
 				      start: 'rotate(0deg) scale( 0.1 )',
 				      stop: 'rotate(720deg) scale( 1 )',
-				      time: i/2,
+				      time: (i/2)-(0.35*i),
 				      duration: 1,
-				      effect:'easeInOut'
+				      effect:'elasticOut'
 				   }
 				}).play();
 			});
@@ -42,46 +64,213 @@ $("#attractLoop").click(function(e) {
 	}).play();
 	
 	$("#attractLoop").slideUp(500);
-	
-	console.log("clicked");
-})
+});
 
+//Select scenario from main menu buttons
 $(".scenarioSelectBtn").click(function(e) {
 	scenario = $(this).attr("category");
 	$("#screenContainer").slideUp( 500, function() {
+		curScreen = 1;
 		loadScenario();
 	  });
-})
+});
 
+//Next Button During Scenarios
+var bindNextBtn = function() {
+	$(".nextBtn").click(function(e) {
+		console.log("NEXT Clicked");
+		$('#screenContainer').tween({
+		   opacity:{
+		      start: 100,
+		      stop: 0,
+		      time: 0,
+		      duration: 0.5,
+		      effect:'sineOut'
+		   },
+		   onStop: function (elem) {
+			  $("#screenContainer").slideUp(25);
+			  curScreen++;
+			  loadScenario();
+		   }
+		}).play();
+	});
+}
+
+// --------------------------- //
+
+//-- Function Vars  -- //
+
+var hardReset = function() {
+	$(document.body).empty().append(initHTML);
+}
+
+//Makes the text bounce at the attract loop
+var loadIdle = function() {
+	$("#loadIdle").tween({
+	  fontSize: {
+			start: 100,
+			stop: 110,
+			time: 0,
+			duration: 0.5,
+			units: 'px',
+			effect:'cubicIn'
+		   },
+	   onStop: function( elem ) {
+			$("#loadIdle").tween({
+				fontSize: {
+					start: 110,
+					stop: 100,
+					time: 0,
+					duration: 0.5,
+					units: 'px',
+					effect:'cubicOut'
+				},
+			   onStop: function( elem ) {
+					loadIdle();
+			   }
+			}).play();
+	   }
+	}).play();
+};
+
+//Called when the user selects their scenario from scenarioSelectBtn.click
 var loadScenario = function() {
-	$("#scenarioSelect").remove();
+	//Hide this stuff
+	$("#scenarioSelect").css("height", "0px");
+	$(".title").css("height", "0px");
 	
+	//load scenario from html into #screnContainer
 	switch (scenario) {
-		case "game":
-			console.log("loadScenario game");
-			$(".title").empty().append("Save for a Game System");
-			
+		case "couch":
+			$("#screenContainer").load("couch.html #couch_screen" + curScreen, function() {
+				console.log("LOAD COUCH.HTML");
+				animateScenario();
+			});
 			break;
 			
 		case "car":
-			console.log("loadScenario car");
-			$(".title").empty().append("Invest in a Car");
+			$("#screenContainer").load("car.html #car_screen" + curScreen, function() {
+				console.log("LOAD CAR.HTML");
+				animateScenario();
+			});
 			break;
 			
-		case "vacation":
-			console.log("loadScenario vacation");
-			$(".title").empty().append("Save for a Vacation");
+		case "savings":
 			break;
 			
-		case "house":
-			console.log("loadScenario house");
-			$(".title").empty().append("Invest in a House");
+		case "lottery":
 			break;
-	}
+	}	
+};
+
+//Tweens the dialog, circles, and images for the scneario expository scenes
+var animateScenario = function() {
+	$('#screenContainer').tween({
+	   opacity:{
+	      start: 0,
+	      stop: 100,
+	      time: 0,
+	      duration: 0,
+	      effect:'sineOut'
+	   }
+	}).play();
 	
-	var comic = "<div id='scenarioSelect'><div class='comicPanel' category='game'>Panel 1</div><div class='comicPanel' category='car'>Panel 2</div><div class='comicPanel clear' category='vacation'>Panel 3</div><div class='comicPanel' category='house'>Panel 4</div></div>";
+	$("#screenContainer").delay(1000).slideDown(500, function() {
 		
-	$("#screenContainer").append(comic);
-	
-	$("#screenContainer").delay(500).slideDown();
+		//Animate image on screen
+		$('.comicImg').tween({
+		   opacity:{
+		      start: 0,
+		      stop: 100,
+		      time: 0,
+		      duration: 0.5,
+		      effect:'sineOut'
+		   },
+		   height:{
+		      start: 0,
+		      stop: 1000,
+		      time: 0,
+		      units: 'px',
+		      duration: 0.5,
+		      effect:'sineOut'
+		   },
+		   top:{
+		      start: 1200,
+		      stop: parseInt($('.comicImg').attr("targetH")),
+		      time: 0,
+		      units: 'px',
+		      duration: 0.5,
+		      effect:'sineOut'
+		   }
+		});
+		
+		//Animate circles onto screen
+			$(".circleText").each( function( i, v ) {
+			  	$( this ).tween({
+				   opacity:{
+				      start: 0,
+				      stop: 100,
+				      time: parseInt($(this).attr("time")),
+				      duration: 0.25,
+				      effect:'easeInOut'
+				   },
+				   transform:{
+				      start: 'rotate(0deg) scale( 0.1 )',
+				      stop: 'rotate(720deg) scale( 1 )',
+				      time: parseInt($(this).attr("time")),
+				      duration: 1,
+				      effect:'elasticOut'
+				   },
+				}).play();
+			});
+					
+		//Animate Text on Screen
+		$(".dialog").each(function(i, v){
+			var thisObj = $(this);
+			var anim = getTargetAnim(thisObj);
+			console.log("anim.timing");
+			console.log(anim.timing);
+			
+			//Set Vertical Location of Dialog
+			$(this).css("top", anim.top + "px");
+			
+			//Animate dialog horizontally
+			$(this).tween({
+			   left:{
+			      start: 1280,
+			      stop: anim.left,
+			      time: parseInt(anim.timing),
+			      units: 'px',
+			      duration: 0.5,
+			      effect:'sineOut'
+			   },
+			   onStop: function( elem ) {
+				
+			   }
+			}).play();
+		});
+		
+		$(".nextBtn").each(function(i, v) {
+			$(this).tween({
+			   opacity:{
+			      	start: 0,
+			      	stop: 100,
+			      	time: parseInt($(this).attr("time")),
+			      	duration: 0.5,
+			      	effect:'sineOut'
+			   },
+			   onStop: function(elem) {
+					bindNextBtn();
+			   }
+			}).play();
+		});
+	});
 }
+
+//Pull anim vars off targeted dialog line
+var getTargetAnim = function(target) {
+	var anim = target.attr("anim");
+	return JSON.parse(anim);
+}
+
+
